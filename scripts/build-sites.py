b326809126a229f68a42ae1cdba2b5da601105f9
王清霓 <429107705@qq.com>
2026-09-09 05:22:30 +0900

    Harden icon previews and theme caching

--- scripts/build-sites.py
+++ scripts/build-sites.py
@@ -13,10 +13,11 @@ source = root / "_site"
 paths = [
     "index.html", "about.html", "about/index.html", "sitemap.xml",
     "assets/css/home.css", "assets/js/theme.js", "assets/mountain.jpg",
-    "assets/bing.jpg", "images/cat-favicon-v2.svg",
-    "images/cat-favicon-v2.ico", "images/cat-touch-v2.png",
-    "images/cat-icon-192-v2.png", "images/cat-icon-512-v2.png",
-    "images/site.webmanifest", "favicon.ico", "public/cat-og-v2.png",
+    "assets/bing.jpg", "images/cat-favicon-16-v3.png",
+    "images/cat-favicon-32-v3.png", "images/cat-favicon-v2.ico",
+    "images/cat-touch-v3.png", "images/cat-icon-192-v3.png",
+    "images/cat-icon-512-v3.png", "images/site.webmanifest",
+    "favicon.ico", "public/cat-og-v3.png",
     "assets/fonts/fa-solid-900.woff2", "assets/fonts/fa-brands-400.woff2",
     "assets/fonts/academicons.woff",
 ]
